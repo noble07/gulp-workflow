d6f350ab2770c34254fbcfff200efaaa23f08614
jcmunoz <jcmunoz@smdigital.com.co>
2019-06-26 08:55:01 -0500

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-// var gulp = require('gulp');
-// var plugin = require('gulp-load-plugins')({ lazy: true });
-
-// plugin.compiler = require('node-sass');
-
-// gulp.task('styles', function(done) {
-//   return gulp.src('scss/app.scss')
-//   .pipe(plugin.sass().on('error', plugin.sass.logError))
-//   .pipe(gulp.dest('app/css'));
-//   done();
-// });
-
-// gulp.task('serve', gulp.series((['styles']), function() {
-//   plugin.init();
-// }))
-
-// gulp.task('default', gulp.series((['styles'])));
-
 var gulp = require('gulp');
+var plugin = require('gulp-load-plugins')({ lazy: true });
+
+plugin.compiler = require('node-sass');
+
+gulp.task('styles', function(done) {
+  return gulp.src('src/scss/app.scss')
+  .pipe(plugin.sass().on('error', plugin.sass.logError))
+  .pipe(gulp.dest('dist/css'));
+  done();
+});
+
+gulp.task('serve', gulp.series((['styles']), function() {
+  plugin.init();
+}))
+
+gulp.task('default', gulp.series((['styles'])));
+
+// var gulp = require('gulp');
